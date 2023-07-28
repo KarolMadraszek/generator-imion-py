03b799bfcb0bb9e98ed54196fcf5fd9d036e9d92
Karol Mądraszek <karol.madraszek.03@gmail.com>
2023-07-28 14:00:24 +0200

    Zamiana plikow z imionami

--- generator-imion.py
+++ generator-imion.py
@@ -1,14 +1,19 @@
 import csv
 import random
 
-class NotSex (Exception):
-    pass
+with open('Popular_Baby_Names.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row)
 
-try:
-    sex = str(input('Podaj swoją płeć (M/K): '))
-    if sex in ['m', 'M', 'k', 'K']:
-        print("Poprawna wartość.")
-    else:
-        raise NotSex
-except NotSex:
-    print('Niepoprawna wartość.')
+# class NotSex (Exception):
+#     pass
+
+# try:
+#     sex = str(input('Podaj swoją płeć (M/K): '))
+#     if sex in ['m', 'M', 'k', 'K']:
+#         print("Poprawna wartość.")
+#     else:
+#         raise NotSex
+# except NotSex:
+#     print('Niepoprawna wartość.')
